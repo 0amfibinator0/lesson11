546ae1bfbdfaf87e5d1061bf58bdc9acad774c63
0amfibinator0 <00amfibiantor00@mail.ru>
2021-10-14 21:56:43 +0300

    задание выполнено

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+'use strict';
 
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+const button = document.getElementById('e_btn');
+const circle = document.getElementById('circle');
+const rangeInput = document.getElementById('range');
+const span = document.getElementById('range-span');
 
+btn.addEventListener('click', function(color) {
+    color = text.value;
+    btn.style.backgroundColor = color;
+});
 
+button.style.display = "none";
 
-
+rangeInput.addEventListener('input', function({target}) {
+    circle.style.width = `${target.value}%`;
+    circle.style.height = `${target.value}%`;
+});
